4d21ea202ef78e7e634023c35d781afb0865995d
ColeKainz <cole.r.kainz@gmail.com>
2021-01-09 13:06:01 -0600

    Hide IBittrexAdapterBase.kt, should only be accessed through factories

--- src/main/kotlin/adapters/bittrex/IBittrexAdapterBase.kt
+++ src/main/kotlin/adapters/bittrex/IBittrexAdapterBase.kt
@@ -3,6 +3,6 @@ package adapters.bittrex
 import ICryonomyAdapter
 import com.bushka.bittrex.BittrexClient
 
-interface IBittrexAdapterBase: ICryonomyAdapter {
+internal interface IBittrexAdapterBase: ICryonomyAdapter {
     val client: BittrexClient
 }
